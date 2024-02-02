48272ba8aa97bf4ab9930ef3a06ccfff656895fb
amitsharma7565 <amitsharma7565@gmail.com>
2024-02-02 17:17:10 +0530

    variable add

--- src/main/java/com/tut/controller/RegisterController.java
+++ src/main/java/com/tut/controller/RegisterController.java
@@ -3,9 +3,11 @@ package com.tut.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tut.model.Register;
@@ -17,7 +19,7 @@ public class RegisterController {
 	
 	@Autowired
 	RegisterInterface registerInterface;
-	
+	@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.POST, RequestMethod.OPTIONS})
 	@PostMapping("/create")
 	public ResponseEntity<Register> registerUser( @RequestBody Register register){
 		Register registerResponse =this.registerInterface.registerUser(register);
--- src/main/java/com/tut/model/Register.java
+++ src/main/java/com/tut/model/Register.java
@@ -15,37 +15,10 @@ public class Register {
 	private String firstName;
 	private String lastName;
 	private String email;
+	private String password;
 	private String phoneNo;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
+	
+	
 	
 	
 }
