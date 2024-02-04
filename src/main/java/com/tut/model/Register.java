a3d6d7ec8ce952b624ea089941728a7ba14a595e
Amit Sharma <amitsharma7565@gmail.com>
2024-02-04 14:14:03 +0530

    model class modify

--- src/main/java/com/tut/model/Register.java
+++ src/main/java/com/tut/model/Register.java
@@ -17,6 +17,47 @@ public class Register {
 	private String email;
 	private String password;
 	private String phoneNo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	@Override
+	public String toString() {
+		return "Register [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", phoneNo=" + phoneNo + "]";
+	}
 	
 	
 	
