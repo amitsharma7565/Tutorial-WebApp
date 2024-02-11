70219dbff05667d40c5f623f808e9b7b3e167c6d
amitsharma7565 <amitsharma7565@gmail.com>
2024-02-11 14:22:25 +0530

    LoginController modify

--- src/main/java/com/tut/controller/LoginController.java
+++ src/main/java/com/tut/controller/LoginController.java
@@ -3,9 +3,11 @@ package com.tut.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tut.Exception.UserNotFoundException;
@@ -17,7 +19,7 @@ import com.tut.service.LoginInterface;
 public class LoginController {
 	@Autowired
 	LoginInterface loginInterface;
-	
+	@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.POST, RequestMethod.OPTIONS})
 	@PostMapping("/user")
 	public ResponseEntity<String> logInUser(@RequestBody Login login) {
 	    try {
