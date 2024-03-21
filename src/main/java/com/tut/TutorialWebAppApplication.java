731102490c211527b4fb3ae92e0aee488d1f6bd6
amitsharma7565 <amitsharma7565@gmail.com>
2024-03-21 21:27:42 +0530

    add server database

--- src/main/java/com/tut/TutorialWebAppApplication.java
+++ src/main/java/com/tut/TutorialWebAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TutorialWebAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("***************Application Start*************");
 		SpringApplication.run(TutorialWebAppApplication.class, args);
+		
 }
 }
